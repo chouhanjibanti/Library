bea4e08d72b9e70de5ea59378d955ee83560ae1a
Banti Chouhan <chouhanbanti224@gmail.com>
2025-07-02 01:33:44 +0530

    updatedfile

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,7 +9,7 @@ import Profile from "./pages/Student/Profile";
 import Sidebar from "./pages/Admin/Sidebar";
 import Dashboard from "./pages/Admin/Dashboard";
 import CourseTable from "./pages/Admin/course/CourseTable";
-import CreateCourse from "./pages/Admin/course/createCourse";
+import CreateCourse from "./pages/Admin/course/CreateCourse";
 import EditCourse from "./pages/Admin/course/EditCourse";
 import CreateLecture from "./pages/Admin/lecture/CreateLecture";
 import EditLecture from "./pages/Admin/lecture/editLecture";
@@ -17,96 +17,128 @@ import ErrorPage from "./pages/ErrorPage";
 import CourseDetail from "./pages/Student/CourseDetail";
 import CourseProgress from "./pages/Student/CourseProgress";
 import SearchPage from "./pages/Student/SearchPage";
-import { AdminOnly, AuthenticatedUser, ProtectedRoutes } from "./components/ProtectedRoutes";
+import {
+  AdminOnly,
+  AuthenticatedUser,
+  ProtectedRoutes,
+} from "./components/ProtectedRoutes";
 import PurchaseCourseProtectedRoute from "./components/PurchaseCourseProtectedRoute";
 
-const router  = createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path: '/',
-    element: <MainLayout/>,
+    path: "/",
+    element: <MainLayout />,
     children: [
       {
-        path: '/',
-       element: (
-        <>
-          <HeroSection/>
-          <Courses/>
-        </>
-       ),
+        path: "/",
+        element: (
+          <>
+            <HeroSection />
+            <Courses />
+          </>
+        ),
       },
       {
-        path: 'login',
-        element: <AuthenticatedUser> <Login/> </AuthenticatedUser>,
-      },{
-        path: 'my-learning',
-        element:<ProtectedRoutes> <Mylearning/> </ProtectedRoutes> 
-      }
-      ,{
-        path: 'profile',
-        element:<ProtectedRoutes> <Profile/> </ProtectedRoutes> 
+        path: "login",
+        element: (
+          <AuthenticatedUser>
+            {" "}
+            <Login />{" "}
+          </AuthenticatedUser>
+        ),
       },
       {
-        path: 'course/search',
-        element: <ProtectedRoutes><SearchPage/></ProtectedRoutes>
+        path: "my-learning",
+        element: (
+          <ProtectedRoutes>
+            {" "}
+            <Mylearning />{" "}
+          </ProtectedRoutes>
+        ),
       },
       {
-         path:"course-detail/:CourseId",
-         element:<ProtectedRoutes><CourseDetail/></ProtectedRoutes>
+        path: "profile",
+        element: (
+          <ProtectedRoutes>
+            {" "}
+            <Profile />{" "}
+          </ProtectedRoutes>
+        ),
       },
       {
-         path:"course-progress/:CourseId",
-         element:<ProtectedRoutes>
-               <PurchaseCourseProtectedRoute>
-                  <CourseProgress/>
-                </PurchaseCourseProtectedRoute>
-             </ProtectedRoutes>
+        path: "course/search",
+        element: (
+          <ProtectedRoutes>
+            <SearchPage />
+          </ProtectedRoutes>
+        ),
       },
-          // admin route
       {
-        path: 'admin',
-        element:<AdminOnly><Sidebar/></AdminOnly>,
+        path: "course-detail/:CourseId",
+        element: (
+          <ProtectedRoutes>
+            <CourseDetail />
+          </ProtectedRoutes>
+        ),
+      },
+      {
+        path: "course-progress/:CourseId",
+        element: (
+          <ProtectedRoutes>
+            <PurchaseCourseProtectedRoute>
+              <CourseProgress />
+            </PurchaseCourseProtectedRoute>
+          </ProtectedRoutes>
+        ),
+      },
+      // admin route
+      {
+        path: "admin",
+        element: (
+          <AdminOnly>
+            <Sidebar />
+          </AdminOnly>
+        ),
         children: [
           {
-            path: 'dashboard',
-            element: <Dashboard/>
+            path: "dashboard",
+            element: <Dashboard />,
           },
           {
-            path: 'course',
-            element: <CourseTable/>
+            path: "course",
+            element: <CourseTable />,
           },
           {
-            path: 'course/create',
-            element: <CreateCourse/>
+            path: "course/create",
+            element: <CreateCourse />,
           },
           {
-            path: 'course/edit/:CourseId',
-            element: <EditCourse/>
+            path: "course/edit/:CourseId",
+            element: <EditCourse />,
           },
           {
-            path: 'course/edit/:CourseId/lecture',
-            element: <CreateLecture/>
+            path: "course/edit/:CourseId/lecture",
+            element: <CreateLecture />,
           },
           {
-            path: 'course/edit/:CourseId/lecture/:LectureId',
-            element: <EditLecture/>
-          }
-        ]
-
-      }
-    ]
-  },{
-    path: '*',
-    element: <ErrorPage/>
-  }
-])
+            path: "course/edit/:CourseId/lecture/:LectureId",
+            element: <EditLecture />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
 function App() {
   const locomotiveScroll = new LocomotiveScroll();
 
   return (
     <>
-     <RouterProvider router={router} >
-
-     </RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
